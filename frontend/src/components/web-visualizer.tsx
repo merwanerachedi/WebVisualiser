87efb7e9a4806ef0cddd189107096ce9fb8faeb2
merwane rachedi <merwane.rachedi@gmail.com>
2025-12-28 20:57:23 +0100

    @ts-expect-error au lieu de ignore

--- frontend/src/components/web-visualizer.tsx
+++ frontend/src/components/web-visualizer.tsx
@@ -636,7 +636,7 @@ export default function WebVisualizer() {
                     className="w-full h-2 bg-white/10 rounded-lg appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:shadow-lg [&::-webkit-slider-thumb]:transition-all [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:cursor-pointer [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:shadow-lg [&::-moz-range-thumb]:transition-all"
                     style={
                       {
-                        // @ts-ignore - CSS custom properties for dynamic colors
+                        // @ts-expect-error - CSS custom properties for dynamic colors
                         "--thumb-color": getSliderThumbColor(importanceFilter),
                       } as React.CSSProperties
                     }
